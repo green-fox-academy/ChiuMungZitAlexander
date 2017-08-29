ea6786c01fb527194293382bb9dcde563bfe2252
AlexanderZhao <zhaomengzhe@qq.com>
2017-08-30 01:52:26 +0800

    Aug.30th

---
+++ jsAcademy/week1/day1/exercise/main.js
@@ -0,0 +1,60 @@
+'use strict'
+
+// EXERCISE_01_HELLO_ME
+console.log("Hello, Alexander!")
+console.log("------------------------------------------------------")
+
+// EXERCISE_02_HUMPTY_DUMPTY
+console.log('Humpty Dumpty sat on a wall,')
+console.log('Humpty Dumpty had a great fall.')
+console.log('All the king\'s horses and all the king\'s men')
+console.log('Couldn\'t put Humpty together again.')
+console.log("------------------------------------------------------")
+
+// EXERCISE_03_HELLO_OTHERS
+console.log("Hello, Chase!")
+console.log("Hello, Zoe!")
+console.log("Hello, Alfred!")
+console.log("------------------------------------------------------")
+
+// EXERCISE_04_INTRODUCE_YOURSELF
+console.log("Alexander Zhao")
+console.log("26")
+console.log("1.72")
+console.log("------------------------------------------------------")
+
+// EXERCISE_05_TWO_NUMBERS
+console.log(22 + 13)
+console.log(22 - 13)
+console.log(13 * 22)
+console.log(22 / 13)
+console.log(22 % 13)
+console.log("------------------------------------------------------")
+
+// EXERCISE_06_CODING_HOURS
+console.log(6 * 5 * 17)
+console.log(Math.round(6 * 5 / 52 * 10000) / 100 + "%")
+console.log("------------------------------------------------------")
+
+// EXERCISE_07_FAVORITE_NUMBER
+var favoriteNumber = 3
+console.log("My favorite number is: " + favoriteNumber)
+console.log("------------------------------------------------------")
+
+// EXERCISE_08_SWAP
+var a = 123
+var b = 526
+var temp = a
+a = b
+b = temp
+console.log(a)
+console.log(b)
+console.log("------------------------------------------------------")
+
+// EXERCISE_09_BMI
+var massInKg = 81.2
+var heightInM = 1.78
+console.log(massInKg / (heightInM * heightInM))
+console.log("------------------------------------------------------")
+
+// EXERCISE_
