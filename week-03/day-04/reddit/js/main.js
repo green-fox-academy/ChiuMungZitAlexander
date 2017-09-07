04e1799b45b048466e2ecb1d5b87fe8d85ad971b
AlexanderZhao <zhaomengzhe@qq.com>
2017-09-08 00:19:50 +0800

    project reddit

--- week-03/day-04/reddit/js/main.js
+++ week-03/day-04/reddit/js/main.js
@@ -9,6 +9,7 @@ PUT /posts/<id> */
 
 const HOST_NAME = "https://time-radish.glitch.me/posts/";
 const POST_LIST_DOM = document.querySelector(".post-content-list");
+const MAX_LIST_LEGNTH = 20;
 var innerHTML1 = '<section class="post"><div class="order-num">';
 var innerHTML2 = '</div><div class="up-down"><div class="up"></div><div class="up-number">';
 var innerHTML3 = '</div><div class="down"></div></div><div class="post-content"><div class="title" title="'
@@ -19,7 +20,7 @@ var postContent = [];
 
 window.onload = function () {
     httpGetRequest();
-    document.querySelector(".post-shelter .post-window").addEventListener("click", function(e) {
+    document.querySelector(".post-shelter .post-window").addEventListener("click", function (e) {
         e.stopPropagation();
     })
 }
@@ -32,18 +33,29 @@ function httpGetRequest() {
         response.json().then(function (postData) {
             postContent = postData.posts.reverse();
             POST_LIST_DOM.innerHTML = "";
-            updateEntirePostList();
+            updateEntirePostList(MAX_LIST_LEGNTH);
         })
     })
 }
 
-function httpPostRequest() {
-
+function httpPostRequest(headerObj) {
+    fetch(HOST_NAME, {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(headerObj),
+    }).then(function (response) {
+        response.json().then(function (data) {
+            httpGetRequest();
+        })
+    })
 }
 
-function updateEntirePostList() {
-    postContent.forEach(function (postObj, index) {
-        createPostList(index, postObj.score, postObj.title, postObj.timestamp);
+function updateEntirePostList(maxlen) {
+    postContent.splice(0, maxlen).forEach(function (postObj, index) {
+        createPostList(index, postObj.score, postObj.title, postObj.timestamp, postObj.owner);
     }, this);
 }
 
@@ -51,8 +63,44 @@ function updateSinglePost() {
 
 }
 
-function createPostList(index, upNumber, title, time) {
+function createPostList(index, upNumber, title, time, owner) {
+    if (!title) {
+        title = "(No Title)";
+    }
+    if (!owner) {
+        owner = "Anonymous";
+    }
     let date = new Date();
-    let leave = date.getTime() - time;
-    POST_LIST_DOM.innerHTML += innerHTML1 + (index + 1) + innerHTML2 + upNumber + innerHTML3 + title + innerHTML3_1 + title + innerHTML4 + leave + innerHTML5;
+    let leaveTime = Math.round((date.getTime() - time) / 1000);
+    if (leaveTime < 60 && leaveTime >= 0) {
+        if (leaveTime === 1) {
+            leaveTime = "submitted " + leaveTime + " second ago by " + owner;
+        } else {
+            leaveTime = "submitted " + leaveTime + " seconds ago by " + owner;
+        }
+    } else if (leaveTime >= 60 && leaveTime < 3600) {
+        leaveTime = Math.round(leaveTime / 60);
+        if (leaveTime === 1) {
+            leaveTime = "submitted " + leaveTime + " minute ago by " + owner;
+        } else {
+            leaveTime = "submitted " + leaveTime + " minutes ago by " + owner;
+        }
+    } else if (leaveTime >= 3600 && leaveTime < 86400) {
+        leaveTime = Math.round(leaveTime / 3600);
+        if (leaveTime === 1) {
+            leaveTime = "submitted " + leaveTime + " hour ago by " + owner;
+        } else {
+            leaveTime = "submitted " + leaveTime + " hours ago by " + owner;
+        }
+    } else if (leaveTime >= 86400) {
+        leaveTime = Math.round(leaveTime / 86400);
+        if (leaveTime === 1) {
+            leaveTime = "submitted " + leaveTime + " day ago by " + owner;
+        } else {
+            leaveTime = "submitted " + leaveTime + " days ago by " + owner;
+        }
+    } else {
+        leaveTime = "submitted just now by " + owner;
+    }
+    POST_LIST_DOM.innerHTML += innerHTML1 + (index + 1) + innerHTML2 + upNumber + innerHTML3 + title + innerHTML3_1 + title + innerHTML4 + leaveTime + innerHTML5;
 }
--- week-03/day-04/reddit/js/popup.js
+++ week-03/day-04/reddit/js/popup.js
@@ -2,6 +2,7 @@ const SHELTER_DOM = document.querySelector(".post-shelter");
 const URL_INPUT_DOM = document.getElementById("url-input");
 const TITLE_INPUT_DOM = document.getElementById("title-input");
 const CHECKBOX_DOM = document.getElementById("anonymus-check");
+const WARNING_DOM = document.querySelector(".warning");
 
 function showPostWindow() {
     SHELTER_DOM.style.display = "block";
@@ -18,5 +19,20 @@ function postReset() {
 }
 
 function postSubmit() {
-
+    WARNING_DOM.innerHTML = "";
+    if (!TITLE_INPUT_DOM.value) {
+        WARNING_DOM.innerHTML = "***Title cannot be empty.***";
+        return;
+    } else {
+        let requestObj = {
+            "title": TITLE_INPUT_DOM.value,
+            "href": URL_INPUT_DOM.value
+        }
+        httpPostRequest(requestObj);
+        hidePostWindow();
+        postReset();
+        let img = document.createElement("img");
+        img.src = "./img/loading.gif";
+        POST_LIST_DOM.appendChild(img);
+    }
 }
