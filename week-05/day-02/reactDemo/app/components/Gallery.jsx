efb3c26345879652c4c3e01847624fa902426cff
AlexanderZhao <zhaomengzhe@qq.com>
2017-09-20 21:27:53 +0800

    add searchbar with function

--- week-05/day-02/reactDemo/app/components/Gallery.jsx
+++ week-05/day-02/reactDemo/app/components/Gallery.jsx
@@ -7,65 +7,90 @@ import KevinDurant from '../images/gallery/KevinDurant.jpg';
 import KobeBryant from '../images/gallery/KobeBryant.jpg';
 import KyrieIrving from '../images/gallery/KyrieIrving.jpg';
 import StephenCurry from '../images/gallery/StephenCurry.jpg';
+import RajonRondo from '../images/gallery/RajonRondo.jpg';
+import ShaqONeal from '../images/gallery/ShaqONeal.jpg';
 import ThumbKawhiLeonard from '../images/thumbnails/KawhiLeonard.jpg';
 import ThumbKevinDurant from '../images/thumbnails/KevinDurant.jpg';
 import ThumbKobeBryant from '../images/thumbnails/KobeBryant.jpg';
 import ThumbKyrieIrving from '../images/thumbnails/KyrieIrving.jpg';
 import ThumbStephenCurry from '../images/thumbnails/StephenCurry.jpg';
+import ThumbRajonRondo from '../images/thumbnails/RajonRondo.jpg';
+import ThumbShaqONeal from '../images/thumbnails/ShaqONeal.jpg';
+
+var imgSrcBackup = [
+    {
+        id: 1,
+        name: 'Kawhi Leonard',
+        url: KawhiLeonard,
+        thumbUrl: ThumbKawhiLeonard,
+        info: 'Kawhi Anthony Leonard (born June 29, 1991) is an American professional basketball player for the San Antonio Spurs of the National Basketball Association (NBA).'
+    },
+    {
+        id: 2,
+        name: 'Kevin Durant',
+        url: KevinDurant,
+        thumbUrl: ThumbKevinDurant,
+        info: 'Kevin Wayne Durant (born September 29, 1988) is an American professional basketball player for the Golden State Warriors of the National Basketball Association (NBA).'
+    },
+    {
+        id: 3,
+        name: 'Kobe Bryant',
+        url: KobeBryant,
+        thumbUrl: ThumbKobeBryant,
+        info: 'Kobe Bean Bryant (born August 23, 1978) is an American retired professional basketball player and businessman. He played his entire 20-year career with the Los Angeles Lakers of the National Basketball Association (NBA).'
+    },
+    {
+        id: 4,
+        name: 'Kyrie Irving',
+        url: KyrieIrving,
+        thumbUrl: ThumbKyrieIrving,
+        info: 'Kyrie Andrew Irving (born March 23, 1992) is an American professional basketball player for the Boston Celtics of the National Basketball Association (NBA).'
+    },
+    {
+        id: 5,
+        name: 'Stephen Curry',
+        url: StephenCurry,
+        thumbUrl: ThumbStephenCurry,
+        info: 'Wardell Stephen Curry II (born March 14, 1988) is an American professional basketball player for the Golden State Warriors of the National Basketball Association (NBA). Many players and analysts have called him the greatest shooter in NBA history.'
+    },
+    {
+        id: 6,
+        name: 'Rajon Rondo',
+        url: RajonRondo,
+        thumbUrl: ThumbRajonRondo,
+        info: ''
+    },
+    {
+        id: 7,
+        name: 'Shaq O\'Neal',
+        url: ShaqONeal,
+        thumbUrl: ThumbShaqONeal,
+        info: ''
+    }
+];
 
 class Gallery extends React.Component {
     constructor(props) {
         super(props);
-        this.imgSrc = [
-            {
-                id: 1,
-                name: 'KawhiLeonard',
-                url: KawhiLeonard,
-                thumbUrl: ThumbKawhiLeonard,
-                info: 'Kawhi Anthony Leonard (born June 29, 1991) is an American professional basketball player for the San Antonio Spurs of the National Basketball Association (NBA).'
-            },
-            {
-                id: 2,
-                name: 'KevinDurant',
-                url: KevinDurant,
-                thumbUrl: ThumbKevinDurant,
-                info: 'Kevin Wayne Durant (born September 29, 1988) is an American professional basketball player for the Golden State Warriors of the National Basketball Association (NBA).'
-            },
-            {
-                id: 3,
-                name: 'KobeBryant',
-                url: KobeBryant,
-                thumbUrl: ThumbKobeBryant,
-                info: 'Kobe Bean Bryant (born August 23, 1978) is an American retired professional basketball player and businessman. He played his entire 20-year career with the Los Angeles Lakers of the National Basketball Association (NBA).'
-            },
-            {
-                id: 4,
-                name: 'KyrieIrving',
-                url: KyrieIrving,
-                thumbUrl: ThumbKyrieIrving,
-                info: 'Kyrie Andrew Irving (born March 23, 1992) is an American professional basketball player for the Boston Celtics of the National Basketball Association (NBA).'
-            },
-            {
-                id: 5,
-                name: 'StephenCurry',
-                url: StephenCurry,
-                thumbUrl: ThumbStephenCurry,
-                info: 'Wardell Stephen Curry II (born March 14, 1988) is an American professional basketball player for the Golden State Warriors of the National Basketball Association (NBA). Many players and analysts have called him the greatest shooter in NBA history.'
-            }
-        ]
         this.state = {
             imgId: 0,
             hoverId: '',
-            searchbarValue: ''
+            searchbarValue: '',
+            imgSrc: imgSrcBackup
         };
+        this.resetState = function () {
+            this.setState({
+                imgSrc: imgSrcBackup
+            });
+        }
     }
 
     arrowClick(num) {
-        if (num === 1 && this.state.imgId < this.imgSrc.length - 1) {
+        if (num === 1 && this.state.imgId < this.state.imgSrc.length - 1) {
             this.setState({
                 imgId: this.state.imgId + 1
             });
-        } else if (num === 1 && this.state.imgId >= this.imgSrc.length - 1) {
+        } else if (num === 1 && this.state.imgId >= this.state.imgSrc.length - 1) {
             this.setState({
                 imgId: 0
             });
@@ -76,7 +101,7 @@ class Gallery extends React.Component {
             });
         } else if (num === -1 && this.state.imgId <= 0) {
             this.setState({
-                imgId: this.imgSrc.length - 1
+                imgId: this.state.imgSrc.length - 1
             });
         }
     }
@@ -99,15 +124,28 @@ class Gallery extends React.Component {
         });
     }
 
-    searchbarChange(event) {
+    searchbarChange(e) {
+        e.persist();
         this.setState({
-            searchbarValue: event.target.value
+            imgSrc: imgSrcBackup
+        }, function () {
+            let reg = new RegExp(e.target.value);
+            let arr = this.state.imgSrc.filter(function (element) {
+                return reg.test(element.name);
+            });
+            this.setState({
+                imgSrc: arr
+            });
         });
+    }
+
+    resetImgId(e) {
+        e.target.value = '';
+        this.resetState();
     }
 
     render() {
         let that = this;
-        var search = that.state.searchbarValue;
         return (
             <main>
                 <section className="gallery">
@@ -122,10 +160,10 @@ class Gallery extends React.Component {
                         </div>
                     </section>
                     <section className="pic">
-                        <img src={this.imgSrc[this.state.imgId].url} alt="images/alt.jpg" />
+                        <img src={this.state.imgSrc[this.state.imgId] !== undefined ? this.state.imgSrc[this.state.imgId].url : ''} alt="" />
                         <div className="description">
-                            <h2>{this.imgSrc[this.state.imgId].name}</h2>
-                            <p>{this.imgSrc[this.state.imgId].info}</p>
+                            <h2>{this.state.imgSrc[this.state.imgId] !== undefined ? this.state.imgSrc[this.state.imgId].name : ''}</h2>
+                            <p>{this.state.imgSrc[this.state.imgId] !== undefined ? this.state.imgSrc[this.state.imgId].info : ''}</p>
                         </div>
                     </section>
                     <section className="rightArrow">
@@ -140,12 +178,12 @@ class Gallery extends React.Component {
                 <section className="nav">
                     <div className="thumbnails">
                         {
-                            this.imgSrc.map(function (item, index) {
+                            this.state.imgSrc.map(function (item, index) {
                                 return (
                                     <nav className={that.state.imgId === item.id - 1 ? 'active' : ''} key={index}
-                                    onMouseEnter={() => that.handleMouseEnter(item.id - 1)}
-                                    onMouseLeave={() => that.handleMouseLeave(item.id - 1)}
-                                    onClick={() => that.thumbnailClick(item.id - 1)}>
+                                        onMouseEnter={() => that.handleMouseEnter(item.id - 1)}
+                                        onMouseLeave={() => that.handleMouseLeave(item.id - 1)}
+                                        onClick={() => that.thumbnailClick(item.id - 1)}>
                                         <div>
                                             <img src={item.thumbUrl} alt="" />
                                         </div>
@@ -158,7 +196,8 @@ class Gallery extends React.Component {
                 </section>
                 <section className="searchbar">
                     <input type="text" placeholder="search player name..."
-                    onChange={() => that.searchbarChange(event)} value={search} />
+                        onChange={that.searchbarChange.bind(this)}
+                        onBlur={that.resetImgId.bind(this)} />
                 </section>
             </main>
         )
