9a32c17d6e8eae664ac838bce98b4a9bcb98be77
AlexanderZhao <zhaomengzhe@qq.com>
2017-09-22 02:53:14 +0800

    gh-lookup project
    add components and scss

--- week-05/day-05/gh-lookup/app/components/Root.jsx
+++ week-05/day-05/gh-lookup/app/components/Root.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import root from '../style/root.scss';
 
 class Root extends React.Component {
@@ -15,6 +16,42 @@ class Root extends React.Component {
                         <label htmlFor="">greenfox-academy/</label>
                         <input type="text" placeholder="repository name" />
                         <button>Go</button>
+                    </div>
+                    <div className="row">
+                        <section className="repo-info">
+                            <div>
+                                <nav>TEACHING-MATERIALS</nav>
+                            </div>
+                            <p className="repo-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p className="last-modified">
+                                <span>Last modified at </span>
+                                <span id="update-time">2017-09-22T02:38:50Z</span>
+                            </p>
+                        </section>
+                        <section className="auth">
+                            <p>Authenticate</p>
+                            <input type="text" placeholder="Username" />
+                            <input type="password" placeholder="Token or password" />
+                            <button>Login</button>
+                        </section>
+                    </div>
+                    <div className="row">
+                        <section className="commits">
+                            <div>
+                                <nav>Commits<span> (2750)</span></nav>
+                            </div>
+                            <div className="single-commit">
+                                <p className="repo-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                <p className="last-modified">
+                                    <span>Alexander</span>
+                                    <span> at </span>
+                                    <span>2017-09-22T02:38:50Z</span>
+                                </p>
+                            </div>
+                        </section>
+                        <section className="recommended">
+                            <p>Recommended</p>
+                        </section>
                     </div>
                 </main>
             </section>
