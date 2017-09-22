b6b7494cb51f1af02c9630bf82a450017323cc4b
AlexanderZhao <zhaomengzhe@qq.com>
2017-09-22 13:00:05 +0800

    completing gh-lookup project
    fix scss, define server and modify index.html

--- week-05/day-05/gh-lookup/app/components/Root.jsx
+++ week-05/day-05/gh-lookup/app/components/Root.jsx
@@ -1,15 +1,88 @@
 import React from 'react';
 
+import server from '../../server/server.js'
+
 import root from '../style/root.scss';
 
+var userStorage = localStorage;
+
 class Root extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            repoInfo: {
+                name: null,
+                description: null,
+                pushed_at: null
+            },
+            name: null,
+            token: null,
+            login: false
+        }
+    }
+    componentWillMount() {
+        /* fetch(server.repoInfo + 'ChiuMungZitAlexander').
+            then((response) => {
+                response.json().
+                    then((data) => {
+                        this.setState({
+                            repoInfo: {
+                                name: data.name,
+                                description: data.description,
+                                pushed_at: data.pushed_at
+                            }
+                        });
+                    });
+            }); */
+    }
+    componentDidMount() {
+        if (!userStorage.username && !userStorage.token) {
+            this.setState({
+                login: true
+            });
+        }
+    }
+    authenticate() {
+        let base64Token = btoa(this.state.name + ':' + this.state.token);
+        /* userStorage.setItem('username', this.state.name);
+        userStorage.setItem('token', btoa(base64Token)); */
+        fetch(server.limitRate, {
+            method: 'GET',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'Basic ' + 'base64Token'
+            }
+        }).then((response) => {
+            if (response.status === 401) {
+                console.log('Wrong username or token!');
+            } else if (response.status === 200) {
+                userStorage.setItem('username', this.state.name);
+                userStorage.setItem('token', btoa(base64Token));
+            }
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+    handleNameChange(e) {
+        e.persist();
+        this.setState({
+            name: e.target.value
+        });
+    }
+    handlePasswordChange(e) {
+        e.persist();
+        this.setState({
+            token: e.target.value
+        });
+    }
     render() {
+        let that = this;
         return (
             <section className="container">
                 <header>
-                    <nav><a href="">GitHub</a></nav>
-                    <nav><a href="">MDN</a></nav>
-                    <nav><a href="">Stack Overflow</a></nav>
+                    <nav><a target="_blank" href="https://github.com/">GitHub</a></nav>
+                    <nav><a target="_blank" href="https://developer.mozilla.org/en-US/#">MDN</a></nav>
+                    <nav><a target="_blank" href="https://stackoverflow.com/">Stack Overflow</a></nav>
                 </header>
                 <main>
                     <div className="searchBox">
@@ -20,19 +93,21 @@ class Root extends React.Component {
                     <div className="row">
                         <section className="repo-info">
                             <div>
-                                <nav>TEACHING-MATERIALS</nav>
+                                <nav>{this.state.repoInfo.name}</nav>
                             </div>
-                            <p className="repo-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p className="repo-description">{this.state.repoInfo.description}</p>
                             <p className="last-modified">
                                 <span>Last modified at </span>
-                                <span id="update-time">2017-09-22T02:38:50Z</span>
+                                <span id="update-time">{this.state.repoInfo.pushed_at}</span>
                             </p>
                         </section>
                         <section className="auth">
                             <p>Authenticate</p>
-                            <input type="text" placeholder="Username" />
-                            <input type="password" placeholder="Token or password" />
-                            <button>Login</button>
+                            <input type="text" id="username" placeholder="Username"
+                                onChange={that.handleNameChange.bind(this)} />
+                            <input type="password" id="password" placeholder="Token or password"
+                                onChange={that.handlePasswordChange.bind(this)} />
+                            <button onClick={that.authenticate.bind(this)}>Login</button>
                         </section>
                     </div>
                     <div className="row">
---
+++ week-05/day-05/gh-lookup/server/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    limitRate: 'https://api.github.com/rate_limit',
+    defaultRepo: 'https://api.github.com/repos/greenfox-academy/ChiuMungZitAlexander',
+    repoInfo: 'https://api.github.com/repos/greenfox-academy/'
+}
